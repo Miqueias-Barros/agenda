fa6fd01f46b14063f94a0046687176abb3b4b22d
Miqueias Barros <103081596+Mikyxx@users.noreply.github.com>
2026-02-20 19:16:22 -0300

    Adição da função de editar e excluir dados

--- agenda/urls.py
+++ agenda/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('login/', views.login_user, name='login'),
     path('agenda/evento/', views.evento),
     path('agenda/evento/submit', views.submit_evento),
+    path('agenda/evento/delete/<int:id_evento>/', views.delete_evento),
     path('login/submit/', views.submit_login, name='submit_login'),
     path('logout/', views.logout_user),
     path('', views.lista_eventos, name='agenda'),
